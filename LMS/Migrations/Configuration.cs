5119a4874f0e38c59c368c2d3df3b4c10dca1e70
Carl Källén <mindthought37@gmail.com>
2017-07-19 11:39:16 +0200

    Added two teachers and one student
    John och Dimitris får känna sig hedrade

--- LMS/Migrations/Configuration.cs
+++ LMS/Migrations/Configuration.cs
@@ -1,7 +1,11 @@
 namespace LMS.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<LMS.Models.ApplicationDbContext>
     {
@@ -36,6 +40,47 @@ namespace LMS.Migrations
                 a => a.Name,
                 new Models.Activity { Name = "C# Basics", Module = context.Modules.Find(1), Type = Models.ActivityType.ELearning, StartTime = new DateTime(2017, 4, 19, 8, 0, 0), EndTime = new DateTime(2017, 04, 19, 12, 0, 0), Description = "The basics of C# on <a href= https://app.pluralsight.com/library/courses/c-sharp-fundamentals-with-visual-studio-2015 > course </a>" }
                 );
+
+            var roleStore = new RoleStore<IdentityRole>(context);
+            var roleManager = new RoleManager<IdentityRole>(roleStore);
+
+            var roleNames = new[] { "Teacher", "Student" };
+            foreach (var roleName in roleNames)
+            {
+                if (!context.Roles.Any(r => r.Name == roleName))
+                {
+                    var role = new IdentityRole { Name = roleName };
+                    var result = roleManager.Create(role);
+                    if (!result.Succeeded)
+                    {
+                        throw new Exception(string.Join("\n", result.Errors));
+                    }
+                }
+            }
+            var userStore = new UserStore<ApplicationUser>(context);
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            var emails = new[] { "john@lexicon.se", "dimitris@lexicon.se", "student@lexicon.se" };
+            foreach (var email in emails)
+            {
+                if (!context.Users.Any(u => u.UserName == email))
+                {
+                    var user = new ApplicationUser { UserName = email, Email = email };
+                    var result = userManager.Create(user, "foobar");
+                    if (!result.Succeeded)
+                    {
+                        throw new System.Exception(string.Join("\n", result.Errors));
+                    }
+                }
+            }
+            var johnUser = userManager.FindByName("john@lexicon.se");
+            userManager.AddToRole(johnUser.Id, "Teacher");
+
+            var dimitrisUser = userManager.FindByName("dimitris@lexicon.se");
+            userManager.AddToRole(dimitrisUser.Id, "Teacher");
+
+            var studentUser = userManager.FindByName("student@lexicon.se");
+            userManager.AddToRole(studentUser.Id, "Student");
         }
     }
 }
