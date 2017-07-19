14ed7ee7d353d480fa2ff92b30c627072f3bd6ef
Carl Källén <mindthought37@gmail.com>
2017-07-19 15:30:11 +0200

    Small Model Changes and Some Course things

--- LMS/Models/Module.cs
+++ LMS/Models/Module.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace LMS.Models
 {
     public class Module
     {
+        [Key]
         public int Id { get; set; }
+        [MaxLength(60)]
         public string Name { get; set; }
         public string Description { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        [Required]
         public Course Course { get; set; }
         public virtual List<Activity> Activities { get; set; }
         //public virtual Document Documents { get; set; } TODO
