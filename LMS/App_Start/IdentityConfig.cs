6b601045f1e2cf792861b427629f09d934e2a1a9
Carl Källén <mindthought37@gmail.com>
2017-08-09 16:38:03 +0200

    Day1
    Set UserLockOutByDefault to false
    Changed default routing to go to courseOverview for teachers and courseDetails for students
    removed unnecessary px from site.css
    Added Teacher-creation to navbar for teachers

--- LMS/App_Start/IdentityConfig.cs
+++ LMS/App_Start/IdentityConfig.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
+﻿using LMS.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
-using LMS.Models;
+using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace LMS
 {
@@ -61,7 +57,7 @@ namespace LMS
             };
 
             // Configure user lockout defaults
-            manager.UserLockoutEnabledByDefault = true;
+            manager.UserLockoutEnabledByDefault = false;
             manager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(5);
             manager.MaxFailedAccessAttemptsBeforeLockout = 5;
 
