5216fcbca4c168c06ec13e793f6d516232abc405
Linus <linus@tollgard.com>
2017-08-15 16:04:37 +0200

    merge

---
+++ LMS/Models/Period.cs
@@ -0,0 +1,13 @@
+﻿namespace LMS.Models
+{
+	public class Period
+	{
+		public int Day { get; set; }
+		public int StartHour { get; set; }
+		public int StartMinute { get; set; }
+		public int EndHour { get; set; }
+		public int EndMinute { get; set; }
+		public string Name { get; set; }
+		public int ModuleId { get; set; }
+	}
+}
