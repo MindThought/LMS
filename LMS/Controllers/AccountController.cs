12c01a243b5acb3bd8d8d799b7899ef2f2a5c351
Alexander Khadji <alexander.khadji@gmail.com>
2017-08-17 17:02:58 +0200

    Name and Login

--- LMS/Controllers/AccountController.cs
+++ LMS/Controllers/AccountController.cs
@@ -74,7 +74,13 @@ namespace LMS.Controllers
 			// This doesn't count login failures towards account lockout
 			// To enable password failures to trigger account lockout, change to shouldLockout: true
 			var result = await SignInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, shouldLockout: false);
-			switch (result)
+            var doesUserExist = await UserManager.FindByEmailAsync(model.Email);
+            if (doesUserExist == null)// || !(await UserManager.IsEmailConfirmedAsync(user.Id)))
+            {
+                // Don't reveal that the user does not exist or is not confirmed
+                return View("NonExistingAccount");
+            }
+            switch (result)
 			{
 				case SignInStatus.Success:
 					var user = await UserManager.FindByEmailAsync(model.Email);
@@ -159,7 +165,7 @@ namespace LMS.Controllers
 			{
 				if (model.courseID == null)
 				{
-					var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+					var user = new ApplicationUser { Name = model.Name, UserName = model.Email, Email = model.Email };
 					var result = await UserManager.CreateAsync(user, model.Password);
 					if (result.Succeeded)
 					{
@@ -171,7 +177,7 @@ namespace LMS.Controllers
 				}
 				else
 				{
-					var user = new ApplicationUser { UserName = model.Email, Email = model.Email, CourseId = int.Parse(model.courseID) };
+					var user = new ApplicationUser { Name = model.Name, UserName = model.Email, Email = model.Email, CourseId = int.Parse(model.courseID) };
 					var result = await UserManager.CreateAsync(user, model.Password);
 					if (result.Succeeded)
 					{
